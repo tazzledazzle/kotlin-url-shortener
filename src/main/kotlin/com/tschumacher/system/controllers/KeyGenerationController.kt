5d6d93e909a672007887707c764062a6c15e68a5
Terence Schumacher <terenceschumacher@gmail.com>
2024-06-20 15:23:07 -0700

    F** - added Key Generation Controller

---
+++ src/main/kotlin/com/tschumacher/system/controllers/KeyGenerationController.kt
@@ -0,0 +1,15 @@
+package com.tschumacher.system.com.tschumacher.system.controllers
+
+import com.tschumacher.system.com.tschumacher.system.services.KeyGenerationService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class KeyGenerationController(private val keyGenerationService: KeyGenerationService) {
+
+    @GetMapping("/generateKey")
+    fun generateKey(@RequestParam length: Int): String {
+        return keyGenerationService.generateKey(length)
+    }
+}
