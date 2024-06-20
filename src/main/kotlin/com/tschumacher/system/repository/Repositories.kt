51d3fd85b2e42561719f9e254483219f30805f36
Terence Schumacher <terenceschumacher@gmail.com>
2024-06-20 16:13:14 -0700

    F** - added Redirection Service url repository

--- src/main/kotlin/com/tschumacher/system/repository/Repositories.kt
+++ src/main/kotlin/com/tschumacher/system/repository/Repositories.kt
@@ -1,9 +1,16 @@
 package com.tschumacher.system.com.tschumacher.system.repository
 
 
+import com.tschumacher.system.com.tschumacher.system.entities.UrlMapping
 import com.tschumacher.system.com.tschumacher.system.entities.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 interface UserRepository : JpaRepository<User, Long>
+
+
+@Repository
+interface UrlMappingRepository : JpaRepository<UrlMapping, Long> {
+    fun findByShortUrl(shortUrl: String): UrlMapping?
+}
